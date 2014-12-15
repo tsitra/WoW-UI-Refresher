3a41ada822950e5baa7da84e2de4b1c6875d7cff
Nathan Brown <nathan@nathansinfo.com>
2014-12-14 19:50:28 -0800

    First Working Version
    Added Ace3 libs too.

---
+++ Refresher.lua
@@ -0,0 +1,21 @@
+﻿Refresher = LibStub("AceAddon-3.0"):NewAddon("Refresher", "AceConsole-3.0");
+Refresher:RegisterChatCommand("refresher", "RefresherSlashProcesserFunc");
+Refresher:RegisterChatCommand("ref", "RefresherSlashProcesserFunc");
+
+local addonVersion = GetAddOnMetadata('Refresher', 'Version');
+
+
+function Refresher:OnInitialize()
+	Refresher:Print("Loading version " .. addonVersion);
+	Refresher:Print("Type /ref or /refresher to reload the UI.");
+end
+
+function Refresher:OnEnable()
+end
+
+function Refresher:OnDisable()
+end
+
+function Refresher:RefresherSlashProcesserFunc(input)
+	ConsoleExec( "reloadui" );
+end
